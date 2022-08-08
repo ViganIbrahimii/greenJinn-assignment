03e1ea70375c98201285840c6dea6ebd6d684cdb
uigiboj <vigan.ibrahimi@speeex.com>
2022-08-08 23:03:37 +0200

    added line chart component and the functionality which  shows  specific trading pair data, refreshes data every 10 sec

--- components/CurrencyChart/CurrencyChart.tsx
+++ components/CurrencyChart/CurrencyChart.tsx
@@ -1,7 +1,79 @@
-import styles from "./CurrencyChart.module.scss";
+import { ResponsiveLine } from "@nivo/line";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import styles from "./CurrencyChart.module.scss";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { ChartDataProps } from "containers/Main/Main.container";
 
-export const CurrencyChart = () => {
-  return <div className={styles.container}></div>;
+export interface CurrencyChartProps {
+  chartType: string;
+  chartData: ChartDataProps;
+  setChartData: Dispatch<SetStateAction<ChartDataProps>>;
+}
+export const CurrencyChart: React.FC<CurrencyChartProps> = ({
+  chartType,
+  chartData,
+  setChartData,
+}) => {
+  let seconds = 0;
+  const fetchChartsData = async () => {
+    if (chartType) {
+      try {
+        const { data } = await axios.get(`/api/specificpair/${chartType}`);
+        if (data) {
+          setChartData((prev: ChartDataProps) => [
+            ...prev,
+            { x: seconds, y: data.last },
+          ]);
+        }
+      } catch (error: any) {
+        console.log(error.message);
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchChartsData();
+    const interval = setInterval(() => {
+      seconds += 10;
+      fetchChartsData();
+    }, 10000);
+
+    return () => clearInterval(interval);
+  }, [chartType]);
+
+  const commonProperties = {
+    margin: { top: 20, right: 20, bottom: 60, left: 80 },
+    animate: true,
+  };
+
+  return (
+    <div className={styles.chartContainer}>
+      {chartData.length > 0 ? (
+        <ResponsiveLine
+          {...commonProperties}
+          data={[{ id: "Value", data: chartData }]}
+          xScale={{
+            type: "linear",
+            min: "auto",
+            max: "auto",
+          }}
+          yScale={{
+            type: "linear",
+            min: "auto",
+            max: "auto",
+          }}
+          axisLeft={{
+            legend: "Value",
+            legendOffset: 12,
+          }}
+          axisBottom={{
+            legend: "Time(seconds)",
+            legendOffset: -12,
+          }}
+        />
+      ) : (
+        <div>avzxxss</div>
+      )}
+    </div>
+  );
 };
---
+++ components/interfaces/TickerValue.ts
@@ -0,0 +1,13 @@
+export default interface FirstTickerValueInterface {
+  volume?: string;
+  last?: string;
+  percent_change_24?: string;
+  timestamp?: string;
+  bid?: string;
+  vwap?: string;
+  high?: string;
+  low?: string;
+  open_24?: string;
+  ask?: string;
+  open?: string;
+}
